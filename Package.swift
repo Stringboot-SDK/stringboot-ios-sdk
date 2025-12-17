fa6a1db80937068ee45e40c1bd5176d515f42c59
Rajan Dube <rajan.reachme@gmail.com>
2025-12-17 19:23:20 +0530

    Update checksum for StringbootSDK binary target

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "StringbootSDK",
             url: "https://github.com/Stringboot-SDK/stringboot-ios-sdk/releases/download/v1.2.1/StringbootSDK.xcframework.zip",
-            checksum: "b044aa10d933c93b97ad9f1c972fec4357ed99b18fa245e610881c1a4a57a4e3"
+            checksum: "1cb263030142f3ebd1eb9a575ea53fd52bc33d2712b465cd26d024127ad23ec8"
         )
     ]
 )
