42305941e679de217f66b9dbb01f168af020ccd0
Rajan Dube <rajan.reachme@gmail.com>
2025-12-26 19:17:07 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -17,7 +17,7 @@ let package = Package(
         .binaryTarget(
             name: "StringbootSDK",
             url: "https://github.com/Stringboot-SDK/stringboot-ios-sdk/releases/download/v1.2.1/StringbootSDK.xcframework.zip",
-            checksum: "1cb263030142f3ebd1eb9a575ea53fd52bc33d2712b465cd26d024127ad23ec8"
+            checksum: "b64497bb8366c827c3b64a6630c53ec81763ec9ac3af2cfa1862d3ec572a900e"
         )
     ]
 )
